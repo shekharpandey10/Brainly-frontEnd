ecd7f77a495be2b7a0941fe226e0ee236e6087d3
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-10-07 20:24:30 +0530

    updated content api base url

--- src/api/ContentApi.ts
+++ src/api/ContentApi.ts
@@ -1,5 +1,5 @@
 
-const BASE_URL = 'http://localhost:4000/app/v1'
+const BASE_URL = 'https://brainly-backend-rw1p.onrender.com/api/v1' 
 
 const token = localStorage.getItem('jwtSecret')
 export const getAllDoc = async () => {
