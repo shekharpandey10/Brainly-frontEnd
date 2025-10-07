4ad07b38378a03cbf3edee9bc9c8bb136b638c66
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-10-07 20:20:35 +0530

    base url updated

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:4000/app/v1'
+const BASE_URL = 'https://brainly-backend-rw1p.onrender.com/' 
 
 export const signUpApi = async (username: string, password: string) => {
   console.log('sign up......')
